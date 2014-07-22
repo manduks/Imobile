b9c5dbcd84e87d94d72b21c5fb033aef29c4afca
miguelsalasmx@gmail.com <voyaserpapa77830>
2014-07-22 12:26:26 -0500

    Se agrega el control del navigation

--- app/controller/phone/Menu.js
+++ app/controller/phone/Menu.js
@@ -5,7 +5,8 @@ Ext.define('APP.controller.phone.Menu', {
     extend: 'Ext.app.Controller',
     config:{
         refs:{
-            loginForm:'menunav menulist'
+            loginForm:'menunav menulist',
+            menuNav:'menunav'
         },
         control:{
             'menunav menulist': {
@@ -18,18 +19,46 @@ Ext.define('APP.controller.phone.Menu', {
         var action = record.data.action;
         switch(action){
             case 'ordenes':
+                this.getMenuNav().push({
+                    title:'ordenes',
+                    html:'ordenes'
+                });
                 break;
             case 'rutas':
+                this.getMenuNav().push({
+                    title:'rutas',
+                    html:'rutas'
+                });
                 break;
             case 'cobranza':
+                this.getMenuNav().push({
+                    title:'cobranza',
+                    html:'cobranza'
+                });
                 break;
             case 'informes':
+                this.getMenuNav().push({
+                    title:'informes',
+                    html:'informes'
+                });
                 break;
             case 'configuracion':
+                this.getMenuNav().push({
+                    title:'configuracion',
+                    html:'configuracion'
+                });
                 break;
             case 'prospectos':
+                this.getMenuNav().push({
+                    title:'prospectos',
+                    html:'prospectos'
+                });
                 break;
             case 'favoritos':
+                this.getMenuNav().push({
+                    title:'favoritos',
+                    html:'favoritos'
+                });
                 break;
             case 'salir':
                 Ext.Viewport.removeAll(true);
